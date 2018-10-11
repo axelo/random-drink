36385c8f171965ef3e9b1ba22e7a209485f9b835
Axel Olsson <axel.olsson@gmail.com>
2018-10-11 19:47:27 +0200

    ui tweaks

--- src/Main.elm
+++ src/Main.elm
@@ -632,30 +632,31 @@ viewDrinkInstructions drink =
                             ]
                     )
             )
-        , div [ class "m-2 mb-8 whitespace-pre-wrap leading-normal" ] [ text drink.instructions ]
+        , div [ class "p-2 pb-8 whitespace-pre-wrap leading-normal" ] [ text drink.instructions ]
         ]
 
 
 viewTopNav prevEnabled =
-    div [ class "flex justify-between m-1 text-sm" ]
+    div [ class "flex justify-between m-1 text-base" ]
         [ button
-            [ class
-                (if not prevEnabled then
-                    "cursor-default text-grey"
+            [ class <|
+                "flex items-center "
+                    ++ (if not prevEnabled then
+                            "cursor-default text-grey"
 
-                 else
-                    "text-blue-dark"
-                )
+                        else
+                            "text-blue-dark"
+                       )
             , disabled (not prevEnabled)
             , onClick GotoPreviousDrink
             ]
-            [ text "< Prev" ]
-        , button [ class "text-blue-dark", onClick GotoNextDrink ] [ text "Next >" ]
+            [ viewChevronLeft, text " Prev" ]
+        , button [ class "flex items-center text-blue-dark", onClick GotoNextDrink ] [ text "Next ", viewChevronRight ]
         ]
 
 
 viewTopBar caption prevEnabled =
-    header [ class "flex-no-shrink pb-1 text-2xl text-center truncate border-b border-grey-light" ]
+    header [ class "flex-no-shrink pb-1 text-3xl text-center truncate" ]
         [ viewTopNav prevEnabled
         , text caption
         ]
@@ -674,7 +675,7 @@ viewTabNav currentRoute =
 
         viewLink svgIcon extraClasses caption url =
             li [ class "" ]
-                [ a [ class (extraClasses ++ " flex flex-col items-center w-24 no-underline p-2"), href url ]
+                [ a [ class (extraClasses ++ " text-xs flex flex-col items-center w-24 no-underline pb-1 pt-2"), href url ]
                     [ svgIcon
                     , text caption
                     ]
@@ -730,16 +731,24 @@ viewSpinner =
 -- Svgs
 
 
+viewChevronLeft =
+    svg [ SvgAttr.viewBox "0 0 185.3 185.3", SvgAttr.class "h-5" ] [ Svg.path [ SvgAttr.fill "currentColor", SvgAttr.stroke "none", SvgAttr.transform "scale(-1, 1) translate(-185.3, 0)", SvgAttr.d "M51.7 185.3a10.7 10.7 0 0 1-7.6-18.3l74.4-74.3L44 18.3A10.7 10.7 0 1 1 59.3 3.1l82 82c4.1 4.2 4.1 11 0 15.2l-82 81.9c-2.1 2-4.8 3.1-7.6 3.1z" ] [] ]
+
+
+viewChevronRight =
+    svg [ SvgAttr.viewBox "0 0 185.3 185.3", SvgAttr.class "h-5" ] [ Svg.path [ SvgAttr.fill "currentColor", SvgAttr.stroke "none", SvgAttr.d "M51.7 185.3a10.7 10.7 0 0 1-7.6-18.3l74.4-74.3L44 18.3A10.7 10.7 0 1 1 59.3 3.1l82 82c4.1 4.2 4.1 11 0 15.2l-82 81.9c-2.1 2-4.8 3.1-7.6 3.1z" ] [] ]
+
+
 viewSvgOverview =
-    svg [ SvgAttr.viewBox "0 0 323.4 323.4", SvgAttr.class "h-8 mb-1" ] [ Svg.path [ SvgAttr.fill "currentColor", SvgAttr.stroke "none", SvgAttr.d "M297.7 53.3c0-35-68.4-53.3-136-53.3s-136 18.3-136 53.3c0 12.1 8.3 22.3 22 30.3l104 101.3v118.5h-47.5a10 10 0 1 0 0 20h115a10 10 0 1 0 0-20h-47.5V185L276 83.5c13.5-8 21.7-18.1 21.7-30.2zM76 32C98.6 24.3 129 20 161.7 20s63 4.3 85.9 12.1c20.8 7.2 30.1 15.8 30.1 21.2 0 5.3-9.3 14-30.1 21.1-22.8 7.8-53.3 12.1-85.9 12.1s-63-4.3-85.8-12C55 67.2 45.7 58.5 45.7 53.2c0-5.4 9.3-14 30.2-21.2zm85.8 134.7l-69-67c21 4.5 45.1 6.8 69 6.8 24 0 48-2.3 69-6.9l-69 67z" ] [] ]
+    svg [ SvgAttr.viewBox "0 0 323.4 323.4", SvgAttr.class "h-6 mb-1" ] [ Svg.path [ SvgAttr.fill "currentColor", SvgAttr.stroke "none", SvgAttr.d "M297.7 53.3c0-35-68.4-53.3-136-53.3s-136 18.3-136 53.3c0 12.1 8.3 22.3 22 30.3l104 101.3v118.5h-47.5a10 10 0 1 0 0 20h115a10 10 0 1 0 0-20h-47.5V185L276 83.5c13.5-8 21.7-18.1 21.7-30.2zM76 32C98.6 24.3 129 20 161.7 20s63 4.3 85.9 12.1c20.8 7.2 30.1 15.8 30.1 21.2 0 5.3-9.3 14-30.1 21.1-22.8 7.8-53.3 12.1-85.9 12.1s-63-4.3-85.8-12C55 67.2 45.7 58.5 45.7 53.2c0-5.4 9.3-14 30.2-21.2zm85.8 134.7l-69-67c21 4.5 45.1 6.8 69 6.8 24 0 48-2.3 69-6.9l-69 67z" ] [] ]
 
 
 viewSvgInstructions =
-    svg [ SvgAttr.viewBox "0 0 411.4 411.4", SvgAttr.class "h-8 mb-1" ] [ Svg.path [ SvgAttr.fill "currentColor", SvgAttr.stroke "none", SvgAttr.d "M248.7 22.1H245V10a10 10 0 0 0-20 0v12.1h-40V10a10 10 0 0 0-20 0v12.1h-40V10a10 10 0 0 0-20 0v12.1H65V10a10 10 0 0 0-20 0v12.1h-3.7c-22 0-40 18-40 40v309.3c0 22 18 40 40 40h207.4c22 0 40-18 40-40V62.1c0-22-18-40-40-40zm20 349.3c0 11-9 20-20 20H41.3c-11 0-20-9-20-20V62.1c0-11 9-20 20-20H45v25.7a10 10 0 0 0 20 0V42.1h40v25.7a10 10 0 0 0 20 0V42.1h40v25.7a10 10 0 0 0 20 0V42.1h40v25.7a10 10 0 0 0 20 0V42.1h3.7c11 0 20 9 20 20v309.3z" ] [], Svg.path [ SvgAttr.fill "currentColor", SvgAttr.stroke "none", SvgAttr.d "M234.5 156.5h-179a10 10 0 0 0 0 20h179a10 10 0 0 0 0-20zM234.5 216.5h-179a10 10 0 0 0 0 20h179a10 10 0 0 0 0-20zM234.5 276.5h-179a10 10 0 1 0 0 20h179a10 10 0 0 0 0-20zM406 42.5a10 10 0 0 0-5.9-8.1 81 81 0 0 0-33.5-8 81 81 0 0 0-33.6 8 10 10 0 0 0-5.8 8.1c-5.5 55-5.5 108.6 0 163.7a10 10 0 0 0 5.6 8l6.8 139.4c0 .7.1 1.3.3 2l11.5 44.2a10 10 0 0 0 9.7 7.5h11c4.5 0 8.5-3.1 9.7-7.5l11.5-44.2.3-2 6.8-139.4a10 10 0 0 0 5.6-8c5.5-55 5.5-108.6 0-163.7zm-59.5 8c3.5-1.4 6.9-2.4 10-3v74.2a10 10 0 0 0 20 0V47.4a69 69 0 0 1 10.2 3c4.5 49.5 4.5 97.7 0 147h-40.3a797.6 797.6 0 0 1 .1-147zm27.1 301l-7 27.2-7-27.1-6.6-134.2h27.2l-6.6 134.2z" ] [] ]
+    svg [ SvgAttr.viewBox "0 0 411.4 411.4", SvgAttr.class "h-6 mb-1" ] [ Svg.path [ SvgAttr.fill "currentColor", SvgAttr.stroke "none", SvgAttr.d "M248.7 22.1H245V10a10 10 0 0 0-20 0v12.1h-40V10a10 10 0 0 0-20 0v12.1h-40V10a10 10 0 0 0-20 0v12.1H65V10a10 10 0 0 0-20 0v12.1h-3.7c-22 0-40 18-40 40v309.3c0 22 18 40 40 40h207.4c22 0 40-18 40-40V62.1c0-22-18-40-40-40zm20 349.3c0 11-9 20-20 20H41.3c-11 0-20-9-20-20V62.1c0-11 9-20 20-20H45v25.7a10 10 0 0 0 20 0V42.1h40v25.7a10 10 0 0 0 20 0V42.1h40v25.7a10 10 0 0 0 20 0V42.1h40v25.7a10 10 0 0 0 20 0V42.1h3.7c11 0 20 9 20 20v309.3z" ] [], Svg.path [ SvgAttr.fill "currentColor", SvgAttr.stroke "none", SvgAttr.d "M234.5 156.5h-179a10 10 0 0 0 0 20h179a10 10 0 0 0 0-20zM234.5 216.5h-179a10 10 0 0 0 0 20h179a10 10 0 0 0 0-20zM234.5 276.5h-179a10 10 0 1 0 0 20h179a10 10 0 0 0 0-20zM406 42.5a10 10 0 0 0-5.9-8.1 81 81 0 0 0-33.5-8 81 81 0 0 0-33.6 8 10 10 0 0 0-5.8 8.1c-5.5 55-5.5 108.6 0 163.7a10 10 0 0 0 5.6 8l6.8 139.4c0 .7.1 1.3.3 2l11.5 44.2a10 10 0 0 0 9.7 7.5h11c4.5 0 8.5-3.1 9.7-7.5l11.5-44.2.3-2 6.8-139.4a10 10 0 0 0 5.6-8c5.5-55 5.5-108.6 0-163.7zm-59.5 8c3.5-1.4 6.9-2.4 10-3v74.2a10 10 0 0 0 20 0V47.4a69 69 0 0 1 10.2 3c4.5 49.5 4.5 97.7 0 147h-40.3a797.6 797.6 0 0 1 .1-147zm27.1 301l-7 27.2-7-27.1-6.6-134.2h27.2l-6.6 134.2z" ] [] ]
 
 
 viewSvgRepo =
-    svg [ SvgAttr.viewBox "0 0 24 24", SvgAttr.class "h-8 mb-1" ] [ Svg.path [ SvgAttr.fill "currentColor", SvgAttr.stroke "none", SvgAttr.d "M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3" ] [] ]
+    svg [ SvgAttr.viewBox "0 0 24 24", SvgAttr.class "h-6 mb-1" ] [ Svg.path [ SvgAttr.fill "currentColor", SvgAttr.stroke "none", SvgAttr.d "M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3" ] [] ]
 
 
 viewSvgSpinner =
